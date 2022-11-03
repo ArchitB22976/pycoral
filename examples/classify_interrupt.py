6c6b888f340c0a35f8fa7d3ca97db129d6c7fc55
Archit <architb2208@gmail.com>
2022-11-03 16:24:58 +0800

    Testing rpi.gpio library

--- examples/classify_interrupt.py
+++ examples/classify_interrupt.py
@@ -1,98 +1,22 @@
-import argparse
+import RPi.GPIO as GPIO
 import time
 
-import pigpio as pg
-import numpy as np
-from PIL import Image
-from pycoral.adapters import classify
-from pycoral.adapters import common
-from pycoral.utils.dataset import read_label_file
-from pycoral.utils.edgetpu import make_interpreter
+# Initialise
+BUTTON_PIN = 26
+GPIO.setmode(GPIO.BCM)
 
-class reader:
-   
-   def __init__(self, pi, gpio, args):
-      
-      self.pi = pi
-      self.gpio = gpio
+def button_callback(channel):
+    print("Button pressed")
 
-      pi.set_mode(gpio, pg.INPUT)
-      pi.set_pull_up_down(gpio, pg.PUD_UP)
-      
-      #add init
-      labels = read_label_file(args.labels) if args.labels else {}
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-      self.interpreter = make_interpreter(*args.model.split('@'))
-      self.interpreter.allocate_tensors()
+GPIO.add_event_detect(
+    BUTTON_PIN, GPIO.RISING, 
+    callback = button_callback, bouncetime = 50
+)
 
-      size = common.input_size(self.interpreter)
-      image = Image.open(args.input).convert('RGB').resize(size, Image.ANTIALIAS)
-      common.set_input(self.interpreter, image) # trading for quant and normals
-
-      self._cb = self.pi.callback(gpio, pg.FALLING_EDGE, self._cbf)
-
-   def run(self):
-       start = time.perf_counter()
-       self.interpreter.invoke()
-       inference_time = time.perf_counter() - start
-       classes = classify.get_classes(self.interpreter, args.top_k, args.threshold)
-       print('%.1fms' % (inference_time * 1000))
-   
-   def _cbf(self, gpio, level, tick):
-
-      if level == 1: # Rising edge.
-            start = time.perf_counter()
-            self.interpreter.invoke()
-            inference_time = time.perf_counter() - start
-            classes = classify.get_classes(self.interpreter, args.top_k, args.threshold)
-            print('%.1fms' % (inference_time * 1000))
-
-pin = pg.pi()
-pin.set_mode(2, pg.OUTPUT) 
-pin.write(2, 0) # Initialise output GPIO (2)
-
-# Parsing arguments from command line to run inference
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument(
-    '-m', '--model', required=True, help='File path of .tflite file.')
-parser.add_argument(
-    '-i', '--input', required=True, help='Image to be classified.')
-parser.add_argument(
-    '-l', '--labels', help='File path of labels file.')
-parser.add_argument(
-    '-k', '--top_k', type=int, default=1,
-    help='Max number of classification results')
-parser.add_argument(
-    '-t', '--threshold', type=float, default=0.0,
-    help='Classification score threshold')
-parser.add_argument(
-    '-c', '--count', type=int, default=5,
-    help='Number of times to run inference')
-parser.add_argument(
-    '-a', '--input_mean', type=float, default=128.0,
-    help='Mean value for input normalization')
-parser.add_argument(
-    '-s', '--input_std', type=float, default=128.0,
-    help='STD value for input normalization')
-args = parser.parse_args()
-
-input = reader(pin, 26, args) # Initialise input GPIO (26)
-
-        
-
-# Running the first inference
-print('Note: The first inference on Edge TPU is slow because it includes',
-        'loading the model into Edge TPU memory.')
-input.run()
-
-print('----Waiting for input----')
-while True:
-    time.sleep(0.01)
-# except KeyboardInterrupt:
-#     print('-------RESULTS--------')
-#     for c in classes:
-#         print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
-
-
-
+try:
+    while True:
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    GPIO.cleanup()
